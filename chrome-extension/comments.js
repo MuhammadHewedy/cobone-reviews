60495864d447bb6732c02f7d7544baa3f44d2460
mohammad <mohammed_a_hewedy@hotmail.com>
2016-06-15 05:42:20 +0300

    refactor

--- chrome-extension/comments.js
+++ chrome-extension/comments.js
@@ -1,4 +1,5 @@
-var serverUrl = "https://cobone-reviews.herokuapp.com"
+//var serverUrl = "https://cobone-reviews.herokuapp.com"
+var serverUrl = "http://localhost:8080"
 
 var buttonList = $(".add-to-cart")
 
@@ -9,7 +10,7 @@ if (buttonList.length <= 0) {
 
     // inject comments html page to html body
     $.get(chrome.extension.getURL('/comments.html'), function(data) {
-        var e = $(_x('//*[@id="page-content-wrapper"]/section[2]/div/div/div[1]'));
+        var e = $(xpath('//*[@id="page-content-wrapper"]/section[2]/div/div/div[1]'));
         $($.parseHTML(data)).appendTo(e);
     });
 
@@ -17,76 +18,86 @@ if (buttonList.length <= 0) {
     var company = $('#page-content-wrapper > section:nth-child(8) > div > div > div.singledealcolright > div.singledealpanel.companyinfo > h3').text()
     var path = window.location.pathname.split("/").join("~").substring(4);
     var getUrl = serverUrl + "/comments/" + path;
+    var submitBound = false;
+    var dealId = null;
 
     console.log(offer, company, getUrl);
 
-    $.get(getUrl, function(data) {
-        console.log(data);
+    loadComments();
 
-        $('#review-name').prop('disabled', false);
-        $('#review-email').prop('disabled', false);
-        $('#review-content').prop('disabled', false);
-        $('#review-submit').prop('disabled', false);
+    /// ------- functions ------------------------------------------------------
 
-        // enable post comment controls
-        var dealId = null;
-        if (data.length > 0) {
-            dealId = data[0].deal.id;
+    // -- load comments function
+    function loadComments() {
+        $.get(getUrl, function(data) {
+            console.log(data);
 
-            // draw comments
-            for (var i = 0; i < data.length; i++) {
-                var comment = data[i];
-                $('#comments').append("<h4>" + comment.name + "</h4>");
-                $('#comments').append("<p>" + comment.content + "</p>");
-                $('#comments').append("<p><small>" + new Date(comment.created).toISOString().slice(0, 10) + "</small></p> <hr />");
-            }
-        } else {
-            $('#comments').append("<h5>No comments found - لا توجد تعليقات</h5>");
-        }
+            // enable post comment controls
+            $('#review-name').prop('disabled', false);
+            $('#review-email').prop('disabled', false);
+            $('#review-content').prop('disabled', false);
+            $('#review-submit').prop('disabled', false);
 
-        $('#review-submit').bind('click', function() {
-
-            if (!$('#review-content').val() || !$('#review-name').val()) {
-                // alert(chrome.i18n.getMessage("comment_required"))
-                alert("Comment and name required - من فضلك أدخل أسمك و تعليقك")
+            if (data.length > 0) {
+                dealId = data[0].deal.id;
+                // draw comments
+                $('#comments').empty()
+                for (var i = 0; i < data.length; i++) {
+                    var comment = data[i];
+                    $('#comments').append("<h4>" + comment.name + "</h4>");
+                    $('#comments').append("<p>" + comment.content + "</p>");
+                    $('#comments').append("<p><small>" + new Date(comment.created).toISOString().slice(0, 10) + "</small></p> <hr />");
+                }
             } else {
-                var comment = {
-                    deal: {
-                        id: dealId,
-                        path: path,
-                        offerTitle: offer,
-                        company: company
-                    },
-                    name: $('#review-name').val(),
-                    email: $('#review-email').val(),
-                    content: $('#review-content').val()
-                };
-
-                $.ajax({
-                    type: 'POST',
-                    url: serverUrl + "/comments",
-                    data: JSON.stringify(comment),
-                    success: function(response) {
-                        successMessage();
-                    },
-                    error: function(jqXHR, textStatus, errorThrown) {
-                        alert('textStatus : ' + textStatus)
-                    },
-                    contentType: "application/json"
-                });
+                $('#comments').append("<h5>No comments found - لا توجد تعليقات</h5>");
             }
 
-            function successMessage() {
-                alert('تم الإرسال بنجاح - success')
-                name: $('#review-name').val('')
-                email: $('#review-email').val('')
-                content: $('#review-content').val('')
-                location.reload();
+            if (!submitBound) {
+                $('#review-submit').bind('click', submit);
+                submitBound = true;
             }
         });
-    });
+    }
 
-    function _x(STR_XPATH) {
+    // -- button submit callback
+    function submit() {
+        if ($('#form')[0].checkValidity()) {
+            var comment = {
+                deal: {
+                    id: dealId,
+                    path: path,
+                    offerTitle: offer,
+                    company: company
+                },
+                name: $('#review-name').val(),
+                email: $('#review-email').val(),
+                content: $('#review-content').val()
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: serverUrl + "/comments",
+                data: JSON.stringify(comment),
+                success: successMessage,
+                error: function(jqXHR, textStatus, errorThrown) {
+                    alert('textStatus : ' + textStatus)
+                },
+                contentType: "application/json"
+            });
+        }
+    }
+
+    // -- success callback
+    function successMessage() {
+        alert('تم الإرسال بنجاح - success')
+        name: $('#review-name').val('')
+        email: $('#review-email').val('')
+        content: $('#review-content').val('')
+        loadComments();
+    }
+
+    // -- xpath utility
+    function xpath(STR_XPATH) {
         var xresult = document.evaluate(STR_XPATH, document, null, XPathResult.ANY_TYPE, null);
         var xnodes = [];
         var xres;
@@ -95,6 +106,4 @@ if (buttonList.length <= 0) {
         }
         return xnodes;
     }
-
-
 }
