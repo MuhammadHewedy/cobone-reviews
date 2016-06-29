83b13755f4e81f03b626a4405c43d7598482c771
mohammad <mohammed_a_hewedy@hotmail.com>
2016-06-29 11:38:32 +0300

    add hibernate logging

--- restart.sh
+++ restart.sh
@@ -15,5 +15,6 @@ else
 fi
 
 #start
+LOG_DIR=/var/app/log
 #note before starting the app, the enviroment variables defined in README.md should be declared in the environment
-nohup java -Djava.security.egd=file:/dev/./urandom -jar java-backend/target/*.jar  &
+nohup java -Djava.security.egd=file:/dev/./urandom -jar java-backend/target/*.jar > $LOG_DIR/cobone-reviews.out &
