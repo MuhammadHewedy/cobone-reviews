ad2901f6a1714e089ee598acb99599d27fe96938
mohammad <mohammed_a_hewedy@hotmail.com>
2016-06-29 11:53:06 +0300

    use tee cmd

--- restart.sh
+++ restart.sh
@@ -17,4 +17,4 @@ fi
 #start
 LOG_DIR=/var/app/log
 #note before starting the app, the enviroment variables defined in README.md should be declared in the environment
-nohup java -Djava.security.egd=file:/dev/./urandom -jar java-backend/target/*.jar > $LOG_DIR/cobone-reviews.out &
+nohup java -Djava.security.egd=file:/dev/./urandom -jar java-backend/target/*.jar | tee $LOG_DIR/cobone-reviews.out &
