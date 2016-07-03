c4c552264365ccbfce0fdb1c9224a13f3552988e
mohammad <mohammed_a_hewedy@hotmail.com>
2016-07-03 06:16:03 +0300

    add init for ios

---
+++ mobile-apps/ios-app/Cobon Reviews/Cobon Reviews/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Cobon Reviews
+//
+//  Created by Muhammad Hewedy on 7/3/16.
+//  Copyright © 2016 Muhammad Hewedy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        webView.loadRequest(NSURLRequest.init(URL: NSURL.init(string: "https://cobone.com")!))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
