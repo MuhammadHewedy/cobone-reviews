5891d0f339a11efe765645d401d83871214569ba
mohammad <mohammed_a_hewedy@hotmail.com>
2016-07-03 16:16:43 +0300

    use SVProgress HUD

--- mobile-apps/ios-app/Cobon Reviews/Cobon Reviews/ViewController.swift
+++ mobile-apps/ios-app/Cobon Reviews/Cobon Reviews/ViewController.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import SVProgressHUD
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
-        webView.loadRequest(NSURLRequest.init(URL: NSURL.init(string: "https://cobone.com")!))
+        self.webView.delegate = self
+        self.webView.loadRequest(NSURLRequest.init(URL: NSURL.init(string: "https://cobone.com")!))
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +26,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    // -- UIWebViewDelegate
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        SVProgressHUD.show()
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        SVProgressHUD.dismiss()
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    }
 }
 
---
+++ mobile-apps/ios-app/Cobon Reviews/Pods/Target Support Files/Pods-Cobon Reviews/Pods-Cobon Reviews-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Cobon_ReviewsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Cobon_ReviewsVersionString[];
+
